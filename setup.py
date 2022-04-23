7556ea765e10c31e53a4dfca6230c0149403718e
Davy Wybiral <davy.wybiral@gmail.com>
2022-04-23 18:37:59 -0500

    add setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='python-aiotor',
+    version='0.0.1',
+    author='Davy Wybiral',
+    author_email="davy.wybiral@gmail.com",
+    description='asyncio Tor controller client',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/wybiral/aiotor",
+    packages=['aiotor'],
+    install_requires=[],
+    license='MIT',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
