f0fb6ed3009bb63b580763fa392d65179b727f27
Davy Wybiral <davy.wybiral@gmail.com>
2022-05-27 10:51:53 -0500

    improve events example

--- examples/events/main.py
+++ examples/events/main.py
@@ -3,8 +3,12 @@ import asyncio
 import sys
 
 # event handler to display bandwidth usage
-async def bw_event(event):
-    print('Bandwidth: read={}, written={}'.format(event.read, event.written))
+async def bw_event(e):
+    print('Bandwidth: read={}, written={}'.format(e.read, e.written))
+
+# event handler to display circuit changes
+async def circ_event(e):
+    print('Circuit: id={}, status={}, path={}'.format(e.id, e.status, e.path))
 
 async def main():
     # connect to tor controller and authenticate
@@ -12,8 +16,10 @@ async def main():
     c = aiotor.Controller(host='127.0.0.1', port=9151)
     await c.connect()
     await c.authenticate()
-    # listen for bandwidth events using bw_event handler
+    # listen for bandwidth update event
     await c.events.on('BW', bw_event)
+    # listen for circuit update event
+    await c.events.on('CIRC', circ_event)
     # run until terminated
     while True:
         await asyncio.sleep(1)
